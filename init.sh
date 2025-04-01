ed57d8b23b8690341db7584173c6866036ceea91
sanmuyan <m@sanmuyan.com>
2025-04-01 20:46:56 +0800

    dev

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-git clone https://github.com/sanmuyan/nvim.git \
-~/.config/nvim
+# 下载 Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# 同步插件
 nvim -u ~/.config/nvim/first_init.lua --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
