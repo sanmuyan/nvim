b00822ce7b639ca2b4a8aefc86b5cf82a160e7e2
sanmuyan <m@sanmuyan.com>
2025-04-28 22:25:59 +0800

    dev

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,7 +3,7 @@ return require('packer').startup(function(use)
     use {'wbthomason/packer.nvim'}
 
     -- AI 补全
-    use {'Exafunction/codeium.vim'}
+    use {'Exafunction/windsurf.vim'}
 
     -- 标签管理
     use {
